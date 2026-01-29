aa960dec378b1e921a1c99277917962be03a46bb
Vamika Singh <vamika19998singh@gmail.com>
2026-01-29 16:08:03 +0530

    User Login implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+
 
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("YouTube Clone API is running");
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,96 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+export const registerUser = async (req, res) => {
+    try {
+
+        const { username, email, password } = req.body;
+
+        //Validate
+
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                message: "Please provide all required fields",
+            });
+        }
+
+        //If user exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists!",
+            });
+        }
+
+        //create new user
+        const user = new User({
+            username,
+            email,
+            password,
+        });
+
+        res.status(201).json({
+            message: "User registered successfully",
+            userId: user._id,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: " Server error during registration",
+
+        });
+    }
+};
+
+export const loginUser = async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        //Validate
+        if(!email || !password) {
+            return res.status(400).json({
+                message: "Email and password are required",
+            });
+        }
+
+        //Find user
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        //Compare password
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch) {
+            return res.status(401).json({
+                message: "Invalid password",
+            });
+        } 
+
+        //Generate JWT
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error during login",
+
+        });
+            
+    }
+};
+
+
