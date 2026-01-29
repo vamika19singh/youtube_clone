d1be8a5393172fba4812d9ad57db9e5f5b43fa4e
Vamika Singh <vamika19998singh@gmail.com>
2026-01-29 11:36:53 +0530

    Added User model with password hashing

---
+++ backend/src/models/User.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.S+$/, "Please enter a valid email address"],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    avatar: {
+        type: String,
+        default: "",
+    },
+    channels: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel",
+    }],
+},
+{ timestamps: true }
+);
+
+// Hash password 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+//Compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
+    
