52002b2026f5d73bffc832ff20c1ebdb69858282
Vamika Singh <vamika19998singh@gmail.com>
2026-01-29 11:24:31 +0530

    Configure MongoDB Atlas connection using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import app from './src/app.js';
+import dotenv from 'dotenv';
+import connectDB from './src/config/db.js';
+
+dotenv.config();
+connectDB();
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);   
