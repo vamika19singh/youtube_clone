40edc9a346ff1f198a0f501023d6fccc58538c90
Vamika Singh <vamika19998singh@gmail.com>
2026-01-30 16:17:23 +0530

    Added APIs to fetch videos for home, search, category and channel

--- backend/src/controllers/videoController.js
+++ backend/src/controllers/videoController.js
@@ -59,3 +59,88 @@ export const createVideo = async (req, res) => {
         });
     }
 };
+
+
+//@desc  Get all videos
+//@route GET /api/videos
+//@access Public
+export const getAllVideos = async (req, res) => {
+    try{
+        const videos = await Video.find()
+        .populate("channel", "channelName")
+        .populate("uploader", "username")
+        .sort({ createdAt: -1 });
+
+        res.status(200).json(videos);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error while fetching videos",
+        });
+    }
+};
+
+//@desc  Get video by category
+//@route GET /api/videos/category/:category
+//@access Public
+export const getVideosByCategory = async (req, res) => {
+    try {
+        const videos = await Video.find({
+        category: req.params.category,
+    })
+        .populate("channel", "channelName")
+        .populate("uploader", "username")
+        .sort({ createdAt: -1 });
+
+        res.status(200).json(videos);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error while fetching videos by category",
+        });
+    }
+};
+
+//@desc  search video by title
+//@route GET /api/videos/search
+//@access Public
+export const searchVideos = async (req, res) => {
+    try {
+        const {q} = req.query;
+        
+        if(!q) {
+            return res.status(400).json({
+                message: "Search query is required",
+            });
+        }
+        const videos = await Video.find({
+            title: { $regex: q, $options: "i" },
+        })
+        .populate("channel", "channelName")
+        .populate("uploader", "username")
+        .sort({ createdAt: -1 });
+
+        res.status(200).json(videos);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error while searching videos",
+        });
+    }
+};
+
+//@desc  Get video of a channel
+//@route GET /api/videos/channel/:channelId
+//@access Public
+export const getVideosByChannel = async (req, res) => {
+    try {
+        const videos = await Video.find({
+            channel: req.params.channelId,
+        })
+        .populate("uploader", "username")
+        .sort({ createdAt: -1 });
+
+        res.status(200).json(videos);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error while fetching channel videos",
+        });
+    }
+};
--- backend/src/routes/videoRoutes.js
+++ backend/src/routes/videoRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { createVideo } from '../controllers/videoController.js';
+import { createVideo, getAllVideos, getVideosByCategory, searchVideos, getVideosByChannel } from '../controllers/videoController.js';
 import protect from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
+// Public routes
+router.get("/", getAllVideos);
+router.get("/search", searchVideos);
+router.get("/category/:category", getVideosByCategory);
+router.get("/channel/:channelId", getVideosByChannel);
 
 // Creating protected route for video
 router.post("/", protect, createVideo);
